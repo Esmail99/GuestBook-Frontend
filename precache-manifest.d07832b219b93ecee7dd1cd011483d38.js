85e0c4289cef1975c5e8aa5e947e7b68f08a1100
Esmail <elmoussel12@gmail.com>
2020-05-15 00:08:00 +0200

    Updates

---
+++ precache-manifest.d07832b219b93ecee7dd1cd011483d38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ead83954685212eab2942b7fae1b10",
+    "url": "/GuestBook-Frontend/index.html"
+  },
+  {
+    "revision": "5fa749031ee1a1c037d2",
+    "url": "/GuestBook-Frontend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4c0abad191ee79f32349",
+    "url": "/GuestBook-Frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5fa749031ee1a1c037d2",
+    "url": "/GuestBook-Frontend/static/js/2.dacd70bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GuestBook-Frontend/static/js/2.dacd70bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c0abad191ee79f32349",
+    "url": "/GuestBook-Frontend/static/js/main.d9ff45c0.chunk.js"
+  },
+  {
+    "revision": "0d083adb137af97774c7",
+    "url": "/GuestBook-Frontend/static/js/runtime-main.6e6a50a1.js"
+  },
+  {
+    "revision": "f1782b1cdcc7d40fd0df91d2e45fed8d",
+    "url": "/GuestBook-Frontend/static/media/delete.f1782b1c.webp"
+  },
+  {
+    "revision": "fb6f568248ca31e456c3904f68bde2aa",
+    "url": "/GuestBook-Frontend/static/media/edit.fb6f5682.png"
+  }
+]);
