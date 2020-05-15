cfc9e5e29483e5c5b92539a8c5bfa24a3fbbab61
Esmail <elmoussel12@gmail.com>
2020-05-15 03:00:08 +0200

    Edit comment styles

--- src/components/homeComponents/Comment.js
+++ src/components/homeComponents/Comment.js
@@ -5,10 +5,10 @@ class Comment extends React.Component {
     const { owner, content } = this.props.comment;
 
     return (
-      <div className="bg-black-20 br3 tl ph3 pa2 pb0 w-80 ml4">
+      <div className="bg-black-20 br3 tl ph3 pa1 ma2 w-80 ml4">
         <p
           onClick={() => this.showProfile(owner)}
-          className="f4 pb0 mb3 mt2 pointer hover-black-70 dark-blue dib"
+          className="f4 pb0 mb0 mt2 pointer hover-black-70 dark-blue dib"
         >
           {owner}
         </p>
